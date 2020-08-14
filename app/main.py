fa6b43b146b4e2f1c2c600d47ea43dddbe9f2021
Raul Dutta <duttar@umich.edu>
2020-08-14 14:54:07 -0400

    make backend do something

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
-from flask import Flask 
+import os
+import json
+import requests
+import flask
+import flask_cors
+import flask_sqlalchemy
+import datetime
 
-app = Flask(__name__) 
+from flask import Flask, request, jsonify, session,make_response, current_app, render_template, json
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+    
+# initializes app
+app = Flask(__name__)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# initializes database
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'db.sqlite')
+
+app.config.from_object(__name__)
+
+CORS(app)
 
 @app.route("/") 
 def home_view(): 
-		return "<h1>Welcome to Geeks for Geeks</h1>"
+		return "hey friends"
 
